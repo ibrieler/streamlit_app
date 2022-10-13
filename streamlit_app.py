a35491ce51de80aeb444d75866ca75e2da97eca6
Isabel Brieler <115727461+ibrieler@users.noreply.github.com>
2022-10-13 12:20:07 -0500

    Update streamlit_app.py
    smoothie multipicker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,19 @@
 import streamlit
 import pandas as pd
 
+# HEADER
 streamlit.title('My Parents New Healthy Diner')
+
+# BREAKFAST MENU
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# SMOOTHIE MENU
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
